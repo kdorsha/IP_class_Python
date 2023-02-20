96797be0ed75c8bb1f6d0471812144a0e1eccedd
kdorsha <124979786+kdorsha@users.noreply.github.com>
2023-02-20 02:25:46 +0000

    Trying to correct a failed push

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-name3 = input('What is your name?\n')
-print('Hi, %s.' % name3)
+#store user entry as a string in file_name variable
+file_name = input("Please enter the file name: ") 
+
+#store user values in set variables 
+name = input("Please enter your name: ") 
+address = input("Please enter your street address: ") 
+phone = input("Please enter your phone number: ") 
+
+#open file set by file_name variable
+with open(file_name, "w") as file:   
+  file.write(f"{name},{address},{phone}\n") 
+  
+#read and display the contents set by file_name variable   
+with open(file_name, "r") as file:   
+    contents = file.read()   
+    print(contents)
